6d7b56efcb55994263a48520615e0827f2c169d5
LiYuanJie-07 <626816858@qq.com>
2019-12-30 11:27:55 +0800

    使用逆向工程

--- src/main/java/com/gdou/controller/LoginController.java
+++ src/main/java/com/gdou/controller/LoginController.java
@@ -4,17 +4,20 @@ import com.gdou.domain.User;
 import com.gdou.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 登录Controller
  */
 @Controller
+@RequestMapping("/loginController")
 public class LoginController extends BasicController{
     //注入UserService
     @Autowired
     private UserService userService;
 
-    public void login(){
+    @RequestMapping("/login")
+    public void login(String username,String password){
         String s = this.md5("123456");
 
         User user = userService.getUserByUserNameAndPassword("admin",s);
